29e69e0ad3564a3d284465b2b38d605683ce8d79
NikS0512 <38032866+NikS0512@users.noreply.github.com>
2019-07-14 14:24:04 +0200

    Dropped from four lists to two lists. Reworked and simplified foreach loops. Calculations work correctly.

--- Star_Labs_Assessment_Sewduth_N/Program.cs
+++ Star_Labs_Assessment_Sewduth_N/Program.cs
@@ -16,15 +16,15 @@ namespace Star_Labs_Assessment_Sewduth_N
         //Method to calculate default price based on size of Product.
         public double DefaultPrice()
         {
-            if (Size == "s")
+            if (Size == "S" || Size =="s")
             {
                 DP = 10;
             }
-            else if (Size == "m")
+            else if (Size == "M" || Size == "m")
             {
                 DP = 20;
             }
-            else if (Size == "l")
+            else if (Size == "L" || Size == "l")
             {
                 DP = 30;
             }
@@ -43,7 +43,6 @@ namespace Star_Labs_Assessment_Sewduth_N
         }
     }
 
-
     class Golfer : Products
     {
         //Method to calculate price based on special parameters. Golfers have their default size price doubled.
@@ -51,9 +50,7 @@ namespace Star_Labs_Assessment_Sewduth_N
         {
             return SP = DefaultPrice() * 2;
         }
-
     }
-
 
     class Jeans : Products
     {
@@ -63,7 +60,6 @@ namespace Star_Labs_Assessment_Sewduth_N
             return SP = DefaultPrice();
         }
     }
-
 
     class FormalPants : Products
     {
@@ -77,81 +73,41 @@ namespace Star_Labs_Assessment_Sewduth_N
     //Basket class to store items and calculate total cost.
     class Basket
     {
-        //List used to store T-shirts.
-        public List<TShirt> TShirts = new List<TShirt>();
+        //List used to store T-shirts and Golfers.
+        public List<Products> Shirts = new List<Products>();
 
-        //List used to store Golfers.
-        public List<Golfer> Golfers = new List<Golfer>();
+        //List used to store Jeans and Formal Pants.
+        public List<Products> Pants = new List<Products>();
 
-        //List used to store Jeans.
-        public List<Jeans> Pants = new List<Jeans>();
-
-        //List used to store Formal Pants.
-        public List<FormalPants> Formal = new List<FormalPants>();
-
-        double Price = 0;
-
-        //Assigns name and size and then calculates cost of TShirt.
-        public double calcTShirt()
-        {
-            foreach (TShirt prod in TShirts)
-            {
-                var tee = new TShirt();
-                tee.Name = prod.Name;
-                tee.Size = prod.Size;
-                Price = tee.SpecialPrice();
-
-            }
-            return Price;
-        }
-
-        //Assigns name and size and then calculates cost of Golfer.
-        public double calcGolfer()
-        {
-            foreach (Golfer prod in Golfers)
-            {
-                Golfer golf = new Golfer();
-                golf.Name = prod.Name;
-                golf.Size = prod.Size;
-                Price = golf.SpecialPrice();
-            }
-            return Price;
-        }
-
-        //Assigns name and size and then calculates cost of Jeans.
-        public double calcJeans()
-        {
-            foreach (Jeans prod in Pants)
-            {
-                var jean = new Jeans();
-                jean.Name = prod.Name;
-                jean.Size = prod.Size;
-                Price = jean.SpecialPrice();
-            }
-            return Price;
-        }
-
-        //Assigns name and size and then calculates cost of Formal pants.
-        public double calcFormal()
-        {
-            foreach (FormalPants prod in Formal)
-            {
-                var formal = new FormalPants();
-                formal.Name = prod.Name;
-                formal.Size = prod.Size;
-                Price = formal.SpecialPrice();
-            }
-            return Price;
-        }
-
-        //Runs methods that calculate item prices and adds them together.
+        //Method that calculates item prices and adds them together.
         public double TotalPrice()
         {
-            double TP = 0;
-            TP = calcTShirt() + calcGolfer() + calcJeans() + calcFormal();
-            return TP;
-        }
+            double Price = 0;
+            foreach (var prod in Shirts)
+            {
+                if (prod is TShirt)
+                {
+                    Price = Price + prod.SpecialPrice();
+                }
+                else if (prod is Golfer)
+                {
+                    Price = Price + prod.SpecialPrice();
+                }
+            }
 
+            foreach (var prod in Pants)
+            {
+                if (prod is Jeans)
+                {
+                    Price = Price + prod.SpecialPrice();
+                }
+                else if (prod is FormalPants)
+                {
+                    Price = Price + prod.SpecialPrice();
+                }
+            }
+            return Price;
+        }
     }
 
     class Program
@@ -162,28 +118,27 @@ namespace Star_Labs_Assessment_Sewduth_N
             var basket = new Basket();
 
             var tshirt = new TShirt();
-            tshirt.Name = "Marvel";
-            tshirt.Size = "s";
-            basket.TShirts.Add(tshirt);
+            tshirt.Name = "Superdry";
+            tshirt.Size = "S";
+            basket.Shirts.Add(tshirt);
 
             var golfer = new Golfer();
             golfer.Name = "Polo";
-            golfer.Size = "l";
-            basket.Golfers.Add(golfer);
+            golfer.Size = "L";
+            basket.Shirts.Add(golfer);
 
             var jeans = new Jeans();
             jeans.Name = "Levis";
-            jeans.Size = "s";
+            jeans.Size = "S";
             basket.Pants.Add(jeans);
 
             var pants = new FormalPants();
             pants.Name = "Gucci";
-            pants.Size = "l";
-            basket.Formal.Add(pants);
+            pants.Size = "L";
+            basket.Pants.Add(pants);
 
             //Runs TotalPrice() and displays total price of items.
             Console.WriteLine("Your total price is R{0}", basket.TotalPrice());
-
             Console.Read();
         }
     }
